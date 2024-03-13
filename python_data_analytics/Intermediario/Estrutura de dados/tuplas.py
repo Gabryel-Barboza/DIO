eaffedb8aad0e79a60d365a1e9a4fb4bb4f4014e
Gabryel_Brz <gabrielbarbosa.alternativa@gmail.com>
2024-03-12 23:10:03 -0300

    feat: add data structure exercises

---
+++ python_data_analytics/Intermediario/Estrutura de dados/tuplas.py
@@ -0,0 +1,20 @@
+# Imutáveis
+frutas = ("laranja", "maçã", "uva",) # Boa prática a vírgula no final. Evita confusões de precedência
+
+letras = tuple("python")
+
+numeros = tuple([1, 2, 3, 4])
+
+pais = ("Brasil",)
+
+print(frutas[0])
+print(frutas[-1])
+
+matriz = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
+print(matriz)
+
+# MÉTODOS
+
+matriz.count()
+matriz.index()
+len(matriz)
