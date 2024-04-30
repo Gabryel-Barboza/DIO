7335730582948125c502ed44015044d7e227093b
Gabryel_Brz <73187678+Gabryel-Barboza@users.noreply.github.com>
2024-04-30 00:38:29 -0300

    Delete python_data_analytics directory

--- python_data_analytics/Basico/print_input.py
+++
@@ -1,6 +0,0 @@
-nome = input("Informe o seu nome: ");
-idade = input("Informe sua idade: ");
-print(nome, idade);
-print(nome, idade, end="...");
-print(nome, idade, end="...\n");
-print(nome, idade, sep="#");
--- python_data_analytics/Basico/tiposdedados.py
+++
@@ -1,4 +0,0 @@
-print(1 + 10);
-print(1.5 + 1 + 2.3);
-print(True);
-print("Python");
