e7e7731ec98d59d12fa6d17125e742eadec3ab3f
Gabryel_Brz <73187678+Gabryel-Barboza@users.noreply.github.com>
2024-04-30 00:39:15 -0300

    Delete python_data_analytics directory

--- python_data_analytics/Basico/identacao_bloco.py
+++
@@ -1,8 +0,0 @@
-
-def sacar(valor):
-    saldo = 500;
-
-    if saldo >= valor:
-        print("Valor sacado");
-
-sacar(500);
--- python_data_analytics/Basico/operadores_associacao.py
+++
@@ -1,8 +0,0 @@
-curso = "Curso de Python";
-frutas = ["laranja", "uva", "limão"];
-saque = [1500, 100];
-
-print("Python" in curso);
-print("maçã" in frutas);
-print(200 not in saque);
-print("Laranja" in frutas);
