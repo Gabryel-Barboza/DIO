8b6d6e17280dc648d5bf44c4456cc226fc2458ea
Gabryel_Brz <gabrielbarbosa.alternativa@gmail.com>
2025-01-24 01:10:48 -0300

    Fixes and features

--- web_developer/nodejs/00_Desafios/01_mario_kart/src/index.js
+++ web_developer/nodejs/00_Desafios/01_mario_kart/src/index.js
@@ -1,5 +1,4 @@
-// Imports
-const readline = require('node:readline');
+const readline = require('readline');
 
 // Objects
 const characters = [
@@ -42,15 +41,15 @@ const characters = [
 ];
 
 // User inputs
-function readInput(message, callback) {
+async function readInput(message) {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  const result = rl.question(message, callback);
+  const result = new Promise((resolve) => rl.question(message, resolve));
+  await result;
   rl.close();
-
   return result;
 }
 
@@ -61,16 +60,17 @@ async function startRace(numPlayers) {
     console.log('Quantidade máxima ultrapassada, até 8 players permitidos!');
     return players;
   } else {
-    for (let c = 1; c < numPlayers; c++) {
-      // Escolhendo personagem - array começa em 0 então indice -1
+    for (let c = 1; c <= numPlayers; c++) {
       // Interpolação de strings|Template strings
-      const characterPlayer = readInput(
-        `P${c} escolha seu personagem: 1 - Mario | 6 - Donkey Kong: `,
-        (index) => characters[index - 1]
+      // Escolhendo personagem - array começa em 0 então indice -1
+      const characterIndex = await readInput(
+        `P${c} escolha seu personagem: 1 - Mario | 6 - Donkey Kong: `
       );
+      const characterPlayer = characters[characterIndex - 1];
 
       const player = {
         nome: `P${c}:${characterPlayer.nome}`,
+        personagem: characterPlayer,
         pontos: 0,
       };
       players.push(player);
@@ -86,20 +86,42 @@ async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-async function getRandomBlock() {
+async function getRandomObject(type) {
   let random = Math.random();
   let result;
 
   // Estrutura condicional switch-case
-  switch (true) {
-    case random < 0.33:
-      result = 'Reta';
-      break;
-    case random < 0.66:
-      result = 'Curva';
-      break;
-    default:
-      result = 'Confronto';
+  if (type === 'block') {
+    switch (true) {
+      case random < 0.33:
+        result = 'Reta';
+        break;
+      case random < 0.66:
+        result = 'Curva';
+        break;
+      default:
+        result = 'Confronto';
+    }
+  } else { // Map
+    switch (true) {
+      case random < 0.16:
+        result = 'Luigi Raceway';
+        break;
+      case random < .32:
+        result = 'Moo Moo Farm';
+        break;
+      case random < .48:
+        result = 'Donut Plains 1';
+        break;
+      case random < .72:
+        result = 'Bowser Castle 1';
+        break;
+      case random < .88:
+        result = 'Peach Beach';
+        break;
+      default:
+        result = 'Yoshi Falls';
+    }
   }
   return result;
 }
@@ -114,112 +136,126 @@ async function logRollResult(characterName, block, diceResult, attribute) {
   );
 }
 
-async function playRaceEngine(character1, character2) {
+async function checkRoundWinner(pointsList) {
+  let maxPoints = 0;
+  let indexPlayer;
+
+  for (let c = 0; c < pointsList.length; c++) {
+    if (pointsList[c] > maxPoints) {
+      maxPoints = pointsList[c];
+      indexPlayer = c;
+    }
+  }
+
+  return indexPlayer;
+}
+
+async function playRaceEngine(players) {
   // Bloco de repetição for
   for (let round = 1; round <= 5; round++) {
+    console.log('==================================================');
     console.log(`Rodada ${round}`);
-    let block = await getRandomBlock();
-    console.log(`Bloco: ${block}`);
+    const block = await getRandomObject('block');
+    const map = await getRandomObject('map');
+    console.log(`--- Bloco: ${block} --- Mapa: ${map} --- `);
 
-    const diceResult1 = await rollDice();
-    const diceResult2 = await rollDice();
-
-    let testSkill1 = 0;
-    let testSkill2 = 0;
-
-    // Estrutura condicional - testes de resultados
-    if (block == 'Reta') {
-      testSkill1 = diceResult1 + character1.velocidade;
-      testSkill2 = diceResult2 + character2.velocidade;
-
-      logRollResult(
-        player1.nome,
-        'VELOCIDADE',
-        diceResult1,
-        player1.velocidade
-      );
-      logRollResult(
-        player2.nome,
-        'VELOCIDADE',
-        diceResult2,
-        player2.velocidade
-      );
-    } else if (block == 'Curva') {
-      testSkill1 = diceResult1 + character1.manobrabilidade;
-      testSkill2 = diceResult2 + character2.manobrabilidade;
-
-      logRollResult(
-        player1.nome,
-        'MANOBRABILIDADE',
-        diceResult1,
-        player1.manobrabilidade
-      );
-      logRollResult(
-        player2.nome,
-        'MANOBRABILIDADE',
-        diceResult2,
-        player2.manobrabilidade
-      );
-    } else {
-      // Confronto
-      let powerResult1 = diceResult1 + character1.poder;
-      let powerResult2 = diceResult2 + character2.poder;
-
-      logRollResult(player1.nome, 'PODER', diceResult1, player1.poder);
-      logRollResult(player2.nome, 'PODER', diceResult2, player2.poder);
-
-      // Verificando quem marcou pontos em confronto
-      if (powerResult1 < powerResult2 && character1.pontos > 0) {
-        console.log(
-          `${character2.nome} ganhou! ${character1.nome} perdeu um ponto.`
-        );
-        character1.pontos--;
-      } else if (powerResult2 < powerResult1 && character2.pontos > 0) {
-        console.log(
-          `${character1.nome} ganhou! ${character2.nome} perdeu um ponto.`
-        );
-        character2.pontos--;
-      }
-
-      // If ternário
-      console.log(powerResult1 === powerResult2 ? 'EMPATE' : '');
+    let diceRolls = [];
+    for (let c = 0; c < players.length; c++) {
+      const diceNumber = await rollDice();
+      diceRolls.push(diceNumber);
     }
 
-    // Verificando o vencedor
-    if (testSkill1 > testSkill2) {
-      console.log(`${player1.nome} marcou um ponto!`);
-      player1.pontos++;
-    } else if (testSkill2 > testSkill1) {
-      console.log(`${player2.nome} marcou um ponto!`);
-      player2.pontos++;
+    let playerPoints = [];
+    let powerPoints = [];
+
+    // Recebendo os pontos de habilidade para cada jogador
+    for (let c = 0; c < players.length; c++) {
+      const character = players[c].personagem;
+
+      // Estrutura condicional - adicionando os pontos de cada player
+      if (block == 'Reta') {
+        logRollResult(
+          players[c].nome,
+          'VELOCIDADE',
+          diceRolls[c],
+          character.velocidade
+        );
+        playerPoints[c] += diceRolls[c] + character.velocidade;
+      } else if (block == 'Curva') {
+        logRollResult(
+          players[c].nome,
+          'MANOBRABILIDADE',
+          diceRolls[c],
+          character.manobrabilidade
+        );
+        playerPoints[c] += diceRolls[c] + character.manobrabilidade;
+      } else {
+        // Confronto
+        logRollResult(players[c].nome, 'PODER', diceRolls[c], character.poder);
+        powerPoints[c] += diceRolls[c] + character.poder;
+      }
+    }
+
+    console.log('==================================================');
+
+    if (block === 'Confronto') {
+      // Verificando quem marcou mais pontos em confronto
+      const indexPlayer = await checkRoundWinner(powerPoints);
+      // Verificando o vencedor do desafio
+      for (let c = 0; c < players.length; c++) {
+        // Clean ifs - pode retirar as chaves se possuir a seguinte estrutura
+        if (c === indexPlayer)
+          console.log(
+            `${players[c].nome} ganhou o confronto! Menos um ponto para os jogadores restantes`
+          );
+        else if (players[c].pontos > 0) players[c].pontos--;
+      }
+    } else {
+      // Verificando quem ganhou os outros desafios.
+      const indexPlayer = await checkRoundWinner(playerPoints);
+
+      for (let c = 0; c < players.length; c++) {
+        if (c === indexPlayer) {
+          console.log(
+            `${players[c].nome} ganhou o desafio! Mais um ponto adicionado ao jogador.`
+          );
+          players[c].pontos++;
+        }
+      }
     }
 
     console.log('==================================================');
   }
+
+  return players;
 }
 
-async function declareWinner(character1, character2) {
+async function declareWinner(players) {
   console.log('Resultado final:');
-  console.log(`${character1.nome}: ${character1.pontos} pontos.`);
-  console.log(`${character2.nome}: ${character2.pontos} pontos.`);
 
-  // Clean ifs - pode retirar as chaves se possuir a seguinte estrutura
-  if (character1.pontos > character2.pontos)
-    console.log(`${character1.nome} venceu a partida!`);
-  else if (character2.pontos > character1.pontos)
-    console.log(`${character2.nome} venceu a partida!`);
-  else console.log('Foi empate!');
+  let maxPoints = 0;
+  let player;
+  for (let c = 0; c < players.length; c++) {
+    if (players[c].pontos > maxPoints) {
+      maxPoints = players[c].pontos;
+      player = players[c];
+    }
+  }
+  console.log(`${player.nome} é o vencedor! Parabéns!!`);
 }
 
 // Auto-invoke functions
 (async function main() {
   // Funções anonimas e arrow functions
+  // Spread operator ... - rest parameter
+  // Ifs ternários
   let players = [];
   players = await startRace(5);
 
-  // if (players) {
-  //   // Functions chains
-  //   await playRaceEngine(player1, player2);
-  //   console.log('Fim!');
-  // }
+  if (players) {
+    // Functions chains
+    await playRaceEngine(players);
+    console.log('Fim!');
+    declareWinner(players);
+  }
 })();
